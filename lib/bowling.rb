859d5534721269d6f30e5c00f4f2dd2449331f1b
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-04-20 22:43:19 +0200

    r Add current roll index

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,10 +1,12 @@
 class Game
   def initialize
-    @rolls = []
+    @rolls = Array.new(21, 0)
+    @current_roll = 0
   end
 
   def roll(knocked_pins)
-    @rolls << knocked_pins
+    @rolls[@current_roll] = knocked_pins
+    @current_roll += 1
   end
 
   def score
