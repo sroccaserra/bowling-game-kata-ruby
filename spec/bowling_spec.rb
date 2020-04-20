8416b8722dd50e5461402e5f650f3883b8b49470
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-04-20 22:35:27 +0200

    r Extract roll_many function

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,17 +1,23 @@
 require './lib/bowling'
 
 RSpec.describe "#score" do
+  def roll_many(nb_rolls, knocked_pins)
+    nb_rolls.times { @game.roll knocked_pins }
+  end
+
   before(:each) do
     @game = Game.new
   end
 
   it "should have a score of zero for a gutter game" do
-    20.times { @game.roll 0 }
+    roll_many 20, 0
+
     expect(@game.score).to eq 0
   end
 
   it "should have a score of 20 for all ones" do
-    20.times { @game.roll 1 }
+    roll_many 20, 1
+
     expect(@game.score).to eq 20
   end
 end
