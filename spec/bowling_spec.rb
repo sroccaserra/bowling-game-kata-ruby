80901938b6a4f0a296aa330166e067755519e024
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-04-20 22:33:23 +0200

    r Extract game creation

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,15 +1,17 @@
 require './lib/bowling'
 
 RSpec.describe "#score" do
+  before(:each) do
+    @game = Game.new
+  end
+
   it "should have a score of zero for a gutter game" do
-    game = Game.new
-    20.times { game.roll 0 }
-    expect(game.score).to eq 0
+    20.times { @game.roll 0 }
+    expect(@game.score).to eq 0
   end
 
   it "should have a score of 20 for all ones" do
-    game = Game.new
-    20.times { game.roll 1 }
-    expect(game.score).to eq 20
+    20.times { @game.roll 1 }
+    expect(@game.score).to eq 20
   end
 end
